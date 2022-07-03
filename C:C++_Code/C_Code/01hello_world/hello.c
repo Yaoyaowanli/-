74429321da4f578a2af5609d8cf500d41606e710
遥远 <102892395+Yaoyaowanli@users.noreply.github.com>
2022-07-03 23:21:09 +0800

    first

---
+++ C:C++_Code/C_Code/01hello_world/hello.c
@@ -0,0 +1,28 @@
+//
+// Created by 姚远 on 2022/7/3.
+//
+
+#include <stdio.h>
+/*int main ()
+{
+    printf("hello world !\n");
+
+    return 0;
+}*/
+
+//数据类型
+
+int main ()
+{
+    //字符类型
+    char ch;
+    ch = 'a';
+    //整型
+    int age = 66;
+    //短整型    long（长整型） long long （长长整型）
+    short num = 10;
+    //浮点型  float（单精度浮点型） double（双精度浮点型）
+    float a = 6.6;
+
+    return 0;
+}
---
+++ C:C++_Code/C_Code/cmake-build-debug/CMakeFiles/C_Code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Code.dir/01hello_world/hello.c.o"
+  "C_Code"
+  "C_Code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
